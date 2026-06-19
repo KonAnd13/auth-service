f4f47c583468ab37bc2cebbb494db8dc58b30ae9
Andrew Konshin <a.mailbox13@gmail.com>
2026-06-19 19:17:01 +0300

    Fixed lombok for dto

--- auth-service-api/src/main/java/org/pharmacy/dto/TokenInfoDto.java
+++ auth-service-api/src/main/java/org/pharmacy/dto/TokenInfoDto.java
@@ -1,13 +1,17 @@
 package org.pharmacy.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Value
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenInfoDto {
-    String token;
-    LocalDate expiredDate;
+    private String token;
+    private LocalDate expiredDate;
 }
